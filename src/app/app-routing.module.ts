83d790d888ebb6361ce6f0fa67decdf1805a2e3b
shahoul95 <shahoul@libheros.fr>
2022-05-21 22:31:42 +0200

    feature(footer.component/ts/html/scss,header.component/tst/html/scss,timeline.component.ts.html.scss,table.component.html/ts/scss): add component header, footer,timeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TimelineComponent} from "./timeline/timeline.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'timeline', pathMatch: 'full' },
+  { path: 'timeline',component: TimelineComponent },
+  { path: 'tabs',component: TimelineComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { TableComponent } from './table/table.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimelineComponent,
+    TableComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
