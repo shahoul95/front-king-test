ebeea4da6c11d6688948d56a7ca0ef4a4d8fc1ac
shahoul95 <shahoul@libheros.fr>
2022-05-22 22:54:03 +0200

    refactor(add type in pipes search-filter)

--- src/app/pipes/search-filter/search-filter.pipe.ts
+++ src/app/pipes/search-filter/search-filter.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {IKingfrench} from "../../interface/Ikingfrench";
 
 @Pipe({
   name: 'searchFilter'
 })
 export class SearchFilterPipe implements PipeTransform {
-  transform(kings: any, args?: any) {
+  transform(kings: Array<IKingfrench>, args?: string) {
    return kings?.filter(function(item: any) {
-     return  JSON.stringify(item).includes(args);
+     return  JSON.stringify(item).includes(<string>args);
    })
   }
 
