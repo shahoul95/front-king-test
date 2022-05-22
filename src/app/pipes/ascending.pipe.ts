4969183f2d1d6bc70d208ab89aef4a6d7dd7cf47
shahoul95 <shahoul@libheros.fr>
2022-05-22 18:54:15 +0200

    feature(asending.pipe.ts,Ikingfrench.ts,king.service.ts): rename folder models vs interface

--- src/app/pipes/ascending.pipe.ts
+++ src/app/pipes/ascending.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {IKingfrench} from "../model/Ikingfrench";
+import {IKingfrench} from "../interface/Ikingfrench";
 
 @Pipe({
   name: 'ascending'
--- src/app/services/king.service.ts
+++ src/app/services/king.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {ApiService} from "./api.service";
 import {Observable} from "rxjs";
-import {IKingfrench} from "../model/Ikingfrench";
+
 @Injectable({
   providedIn: 'root'
 })
